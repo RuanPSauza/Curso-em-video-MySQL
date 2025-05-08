5dd51bbdcda7485152309f0a292f19bbaf8ef5fa
Ruan souza <ruanpereira020@gmail.com>
2025-05-08 16:34:14 -0300

    Create Inserindo dados.sql
    Aprendendo a inserir os dados no banco com o comando INSERT INTO

---
+++ Inserindo dados.sql
@@ -0,0 +1,8 @@
+
+insert into tb_pessoas 
+(pes_id, pes_nome, pes_nascimento, pes_sexo, pes_peso, pes_altura, pes_nacionalidade) 
+values
+(default, 'Caio', '2007-7-10', 'M', '90kg', '1,80', 'Brasil');
+
+
+select * from tb_pessoas;
